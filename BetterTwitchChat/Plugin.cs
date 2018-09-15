4d86c4a38d87e3f74cad3d0f8cf67afa283fc9a5
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-09-15 12:55:59 -0500

    update version

--- BetterTwitchChat/Plugin.cs
+++ BetterTwitchChat/Plugin.cs
@@ -22,7 +22,7 @@ using BetterTwitchChat.UI;
 namespace BetterTwitchChat {
     public class Plugin : IPlugin {
         public string Name => "BetterTwitchChat";
-        public string Version => "0.2.0";
+        public string Version => "0.2.1";
 
         public bool IsAtMainMenu = false;
         public bool ShouldWriteConfig = false;
