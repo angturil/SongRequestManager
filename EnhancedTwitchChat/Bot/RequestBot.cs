bd7806fe2c137216598550e098f5096340509f61
Michael Linov <angturil@sympatico.ca>
2019-03-04 10:47:38 -0500

    there appears to be an existing !currentsong bot active, so I disabled my aliases for it.

--- EnhancedTwitchChat/Bot/RequestBot.cs
+++ EnhancedTwitchChat/Bot/RequestBot.cs
@@ -565,8 +565,8 @@ namespace EnhancedTwitchChat.Bot
             Commands.Add("clearalreadyplayed", ClearDuplicateList); // Needs a better name
 
             Commands.Add("link", ShowSongLink);
-            Commands.Add("current", ShowSongLink);
-            Commands.Add("currentsong", ShowSongLink);
+            //Commands.Add("currentsong", ShowSongLink);
+
 
 #if PRIVATE
             Commands.Add("goodmappers",mapperWhitelist);
