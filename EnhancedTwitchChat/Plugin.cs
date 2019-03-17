1c24271c4943676cf80d63b27c7ccdd40ad69f53
brian91292 <8050523+brian91292@users.noreply.github.com>
2019-03-16 22:58:48 -0500

    beta1

--- EnhancedTwitchChat/Plugin.cs
+++ EnhancedTwitchChat/Plugin.cs
@@ -19,7 +19,7 @@ namespace EnhancedTwitchChat
     public class Plugin : IPlugin
     {
         public string Name => "EnhancedTwitchChat";
-        public string Version => "1.2.0";
+        public string Version => "1.2.0-beta1";
 
         public bool IsAtMainMenu = true;
         public bool IsApplicationExiting = false;
