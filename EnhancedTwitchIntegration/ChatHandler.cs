aada6fa85db00762da7792df326e0bad51341912
krayn <3692964+krayn@users.noreply.github.com>
2022-03-17 09:41:53 +0100

    rudimentary websocket integration - broken requestbotconfig with WS request

--- EnhancedTwitchIntegration/ChatHandler.cs
+++ EnhancedTwitchIntegration/ChatHandler.cs
@@ -2,6 +2,8 @@
 using ChatCore;
 using ChatCore.Models.Twitch;
 using ChatCore.Services.Twitch;
+using WebSocketSharp;
+using Newtonsoft.Json;
 
 namespace SongRequestManager
 {
@@ -11,16 +13,50 @@ namespace SongRequestManager
 
         private static ChatCoreInstance _sc;
         private static TwitchService _twitchService;
+        private static WebSocket _ws;
 
         public void Awake()
         {
             DontDestroyOnLoad(gameObject);
         }
 
+        public class IWebSocketMessage
+        {
+            
+            public string Id { get;  set; }
+
+            public string UserName { get;  set; }
+
+            public string DisplayName { get;  set; }
+
+            public string Color { get;  set; }
+
+            public bool IsModerator { get;  set; }
+
+            public bool IsBroadcaster { get;  set; }
+
+            public bool IsSubscriber { get;  set; }
+
+            public bool IsTurbo { get;  set; }
+
+            public bool IsVip { get;  set; }
+
+            public ChatCore.Interfaces.IChatBadge[] Badges { get; set; }
+
+            public string Message { get; set; }
+        }
+        
         public void Init()
         {
             if (initialized) return;
+            _ws = new WebSocket("ws://127.0.0.1:9090/SRM");
+            _ws.OnMessage += _ws_OnTextMessageReceived;
 
+            _ws.OnClose += (sender, args) =>
+            {
+                _ws.Connect();
+            };
+            _ws.Connect();
             // create chat core instance
             _sc = ChatCoreInstance.Create();
 
@@ -37,9 +73,19 @@ namespace SongRequestManager
             RequestBot.COMMAND.Parse((TwitchUser)msg.Sender, msg.Message);
         }
 
+        
+        private void _ws_OnTextMessageReceived(object s, MessageEventArgs e)
+        {
+                
+            Plugin.Log("recv: " + e.Data);
+            IWebSocketMessage msg = JsonConvert.DeserializeObject<IWebSocketMessage>(e.Data);
+            var sender = new TwitchUser(e.Data);
+            RequestBot.COMMAND.Parse(sender, msg.Message);
+        }
+        
         public static TwitchUser Self => _twitchService.LoggedInUser;
 
-        public static bool Connected => _twitchService.LoggedInUser != null && _twitchService.Channels.Count > 0;
+        public static bool Connected => (_twitchService.LoggedInUser != null && _twitchService.Channels.Count > 0) || _ws.IsAlive;
 
         public static void Send(string message, bool isCommand = false)
         {
@@ -57,6 +103,7 @@ namespace SongRequestManager
                         _twitchService.SendTextMessage(message, channel.Value);
                     }
                 }
+                _ws.Send(message);
             }
             catch (Exception e)
             {
